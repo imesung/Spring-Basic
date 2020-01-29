feafb9cea886f095924ce7de942ab2a4f6bf4c10
mesung <mesung@mesungui-MacBookPro.local>
2020-01-30 01:12:09 +0900

    proxy aop

---
+++ Spring-AOP/src/main/java/com/mesung/Spring/EventService.java
@@ -0,0 +1,8 @@
+package com.mesung.Spring;
+
+//Subject
+public interface EventService {
+
+    void createEvent();
+    void publishEvent();
+}
---
+++ Spring-AOP/src/main/java/com/mesung/Spring/SimpleEventService.java
@@ -0,0 +1,28 @@
+package com.mesung.Spring;
+
+import org.springframework.stereotype.Service;
+
+//Real Subject
+@Service
+public class SimpleEventService implements EventService {
+    @Override
+    public void createEvent() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Created an event");
+
+    }
+
+    @Override
+    public void publishEvent() {
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Published an event");
+    }
+}
