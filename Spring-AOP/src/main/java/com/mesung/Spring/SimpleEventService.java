884cf9a828943b6652e16727c932c6a93a47cbfc
mesung <myhs2002@gmail.com>
2020-01-30 01:19:00 +0900

    proxy aop study

--- Spring-AOP/src/main/java/com/mesung/Spring/SimpleEventService.java
+++ Spring-AOP/src/main/java/com/mesung/Spring/SimpleEventService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class SimpleEventService implements EventService {
     @Override
     public void createEvent() {
+        long begin = System.currentTimeMillis();
         try {
             Thread.sleep(1000L);
         } catch (InterruptedException e) {
@@ -14,15 +15,19 @@ public class SimpleEventService implements EventService {
         }
         System.out.println("Created an event");
 
+        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Override
     public void publishEvent() {
+        long begin = System.currentTimeMillis();
         try {
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("Published an event");
+
+        System.out.println(System.currentTimeMillis() - begin);
     }
 }
